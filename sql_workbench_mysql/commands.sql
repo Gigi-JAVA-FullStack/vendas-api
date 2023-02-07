8047b43a1ca1524ae9ec1319b173a3a900f34355
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-02-07 15:04:03 -0300

    resources

---
+++ sql_workbench_mysql/commands.sql
@@ -0,0 +1,14 @@
+
+USE vendas_db;
+
+show tables;
+
+DESCRIBE produto;
+DESCRIBE cliente;
+DESCRIBE item;
+DESCRIBE venda;
+
+SELECT * FROM produto;
+SELECT * FROM cliente;
+SELECT * FROM item;
+SELECT * FROM venda;
